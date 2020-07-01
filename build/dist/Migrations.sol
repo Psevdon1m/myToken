4f9c9c1c03ff52f2dd702403eb53ad3f82053649
Valentyn <valentine.oleynik@gmail.com>
2020-07-01 14:16:46 +0300

    practiced infura sol-merger truffle-hd-wallet

---
+++ build/dist/Migrations.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.6.0;
+
+
+// SPDX-License-Identifier: MIT
+contract Migrations {
+    address public owner;
+    uint256 public last_completed_migration;
+
+    function Migration() public {
+        owner = msg.sender;
+    }
+
+    modifier restricted() {
+        if (msg.sender == owner) _;
+    }
+
+    function setCompleted(uint256 completed) public restricted {
+        last_completed_migration = completed;
+    }
+}
