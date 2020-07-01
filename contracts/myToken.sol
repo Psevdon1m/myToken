20fb4d08974892207cad4aa9b547a8048730919a
Valentyn <valentine.oleynik@gmail.com>
2020-07-01 11:16:16 +0300

    using safemath added, allowance added, small bugs fixed

--- contracts/myToken.sol
+++ contracts/myToken.sol
@@ -1,51 +1,62 @@
 pragma solidity ^0.6.0;
 
-contract SafeMath {
-  //internals
+import 'github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/math/SafeMath.sol';
 
-  function safeMul(uint a, uint b) internal pure returns (uint) {
-    uint c = a * b;
-    assert(a == 0 || c / a == b);
-    return c;
-  }
+// contract SafeMath {
+//   //internals
 
-  function safeSub(uint a, uint b) internal pure returns (uint) {
-    assert(b <= a);
-    return a - b;
-  }
+//   function safeMul(uint a, uint b) internal pure returns (uint) {
+//     uint c = a * b;
+//     assert(a == 0 || c / a == b);
+//     return c;
+//   }
 
-  function safeAdd(uint a, uint b) internal pure returns (uint) {
-    uint c = a + b;
-    assert(c>=a && c>=b);
-    return c;
-  }
+//   function safeSub(uint a, uint b) internal pure returns (uint) {
+//     assert(b <= a);
+//     return a - b;
+//   }
 
-  function assert(bool assertion)  internal pure {
-    if (!assertion) revert('Asser Error');
-  }
-}
+//   function safeAdd(uint a, uint b) internal pure returns (uint) {
+//     uint c = a + b;
+//     assert(c>=a && c>=b);
+//     return c;
+//   }
 
-contract MyToken is SafeMath {
-    string public name = "ValTokenBurn";
-    string public symbol = "VLTB";
+//   function assert(bool assertion)  internal pure {
+//     if (!assertion) revert('Asser Error');
+//   }
+// }
+
+contract MyToken {
+    
+    using SafeMath for uint256;
+    
+    string public name = "ValTokenBurnFull";
+    string public symbol = "VLTBF";
     uint256 public totalSupply;
     uint256 public feePerTransaction = 1;
     address private owner;
     uint8 public decimals = 0;
 
-    //from this time on tokens may be burned +24 hours from 29.06.20
+    
+
+    //from this time on tokens may be burned +24 hours from 01.07.20
     uint256 public burnStartTime = now + 24 hours;
 
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
     event Burned(uint256 amount);
 
     mapping(address => uint256) public balanceOf;
+
+    mapping(address => mapping(address => uint256)) public allowance;
 
     constructor(uint256 _initialSupply) public {
         owner = msg.sender;
         balanceOf[msg.sender] = _initialSupply;
         totalSupply = _initialSupply;
         //allocate initial supply
+        emit Transfer(address(0), owner, _initialSupply);
     }
 
     //Transfer function
@@ -55,9 +66,9 @@ contract MyToken is SafeMath {
         returns (bool success)
     {
         require(balanceOf[msg.sender] >= _value, 'error, balance is insufficient');
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value - feePerTransaction;
-        balance[owner] += feePerTransaction;
+        balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+        balanceOf[_to] = balanceOf[_to].add(_value.sub(feePerTransaction));
+        balanceOf[owner] = balanceOf[owner].add(feePerTransaction);
         //transfer event
         emit Transfer(msg.sender, _to, _value);
         return true;
@@ -68,6 +79,29 @@ contract MyToken is SafeMath {
         _;
     }
 
+    //allowance implementation
+  
+
+    function approve(address _spender, uint256 _value) public returns (bool success) {
+      allowance[msg.sender][_spender] = _value;
+
+      //will trigger the approve event
+      emit Approval(msg.sender, _spender, _value);
+      return true;
+  }
+
+    function transferFrom(address _from, address _to, uint256 _value) public returns(bool) {
+      require(balanceOf[_from] >= _value, "insufficient balance");
+      require(allowance[_from][msg.sender] >= _value, "allowance exceeded");
+      balanceOf[_from] =  balanceOf[_from].sub(_value);
+      balanceOf[_to] = balanceOf[_to].add(_value);
+      allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+      emit Transfer(_from, _to, _value);
+      return true;
+    }
+
+
+
     // burnign function
    function burn(uint256 _value)  public onlyOwner returns (bool success)  {
        //we restrict ourseleves to burn tokens after some perion of time.
@@ -75,9 +109,9 @@ contract MyToken is SafeMath {
            //checking that amount is less or equal to user's balance
            require(balanceOf[msg.sender] >= _value, 'balance insufficient');
            //decreasing the users balance by the amount
-           balanceOf[msg.sender] = safeSub(balanceOf[msg.sender], _value);
+           balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
             //decreasing the totalSupply by the amount
-           totalSupply = safeSub(totalSupply, _value);
+           totalSupply = totalSupply.sub(_value);
            emit Burned(_value);
            return true;
        }
